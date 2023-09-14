ef8f8fb37a3d986f1372960cdf0e4a8f13440ece
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-09-14 14:01:18 -0300

    feat: recebimento do cep por query params e pequena validação com throw

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
 import { enrollmentsService } from '@/services';
+import { invalidDataError } from '../errors';
 
 export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
@@ -22,6 +23,7 @@ export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, re
 
 // TODO - Receber o CEP do usuário por query params.
 export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
+  if(req.query.cep.length!==8) throw invalidDataError("cep fornecido invalido")
   const address = await enrollmentsService.getAddressFromCEP();
   res.status(httpStatus.OK).send(address);
 }
