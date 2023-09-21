e6508e2cf4c2409cfe07830a61c2a25c7095f6ed
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-09-21 17:37:33 -0300

    implementado a verificação se existe enrollment no id do user para mostrar se tem ou não ticket, se tiver mostra se não da 404

--- src/services/ticket-service.ts
+++ src/services/ticket-service.ts
@@ -1,6 +1,10 @@
 import { ticketRepository } from "../repositories/ticket-repository";
+import { enrollmentRepository } from "../repositories";
+import { notFoundError } from "../errors";
 
 async function getTicket(userId: any){
+    const user = await enrollmentRepository.findWithAddressByUserId(userId)
+    if(user === null) throw notFoundError();
     const result = await ticketRepository.getTicket(userId);
     return result
 }
