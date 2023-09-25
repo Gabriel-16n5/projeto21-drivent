0f0eae5f2ae7ab04e22795924e4313cfe1a03e67
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-09-25 17:19:38 -0300

    done

--- src/controllers/payments-controller.ts
+++ src/controllers/payments-controller.ts
@@ -10,6 +10,6 @@ export async function getPayment(req: AuthenticatedRequest,  res: Response){
     const ticketId:number = parseInt(req.query.ticketId as string)
     if(!ticketId) throw badRequestError()
     const payment = await paymentRepository.getPayment(ticketId);
-    if(payment === null ) throw notFoundError()
+    if(payment === null ) throw notFoundError();
     return res.status(httpStatus.OK).send(payment)
 }
